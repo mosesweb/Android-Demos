a0ab236262e1c02b93ebb4465b3d26f22e2972ea
Moses Johansson <mosesjohansson@gmail.com>
2016-11-29 21:47:42 +0100

    new activity run when button is pressed

---
+++ app/src/main/java/com/example/studerande/labb3/DisplayMessageActivity.java
@@ -0,0 +1,25 @@
+package com.example.studerande.labb3;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class DisplayMessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_message);
+
+        Intent intent = getIntent(); // Needed because Activity is invoked by an Intent. Regardless of how the user navigated there.
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE); // Retreives the data from the first activity
+        TextView textView = new TextView(this);
+        textView.setTextSize(40);
+        textView.setText(message);
+
+        ViewGroup layout = (ViewGroup) findViewById(R.id.activity_display_message);
+        layout.addView(textView);
+    }
+}
--- app/src/main/java/com/example/studerande/labb3/MainActivity.java
+++ app/src/main/java/com/example/studerande/labb3/MainActivity.java
@@ -1,13 +1,28 @@
 package com.example.studerande.labb3;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    public final static String EXTRA_MESSAGE = "com.example.studerande.labb3.MESSAGE";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /* When an user click on the send button */
+    public void sendMessage(View view)
+    {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 }
